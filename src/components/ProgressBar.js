b95f05dcf82fd44c274c2d3b36a3bd802b525fc9
zac1st1k <zacfirst@gmail.com>
2015-07-30 21:21:15 +1000

    + HTML5 progress bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -17,13 +17,54 @@ var ProgressBar = React.createClass({
   componentDidUpdate: function() {},
   componentWillUnmount: function() {},
 
+  handleAddClick: function() {
+    var option = React.findDOMNode(this.refs.select).value;
+    switch(option) {
+      case '1':
+        var bar1 = React.findDOMNode(this.refs.progressBar1);
+        bar1.value = bar1.value + 10;
+        break;
+      case '2':
+        var bar2 = React.findDOMNode(this.refs.progressBar2);
+        bar2.value = bar2.value + 10;
+        break;
+      case '3':
+        var bar3 = React.findDOMNode(this.refs.progressBar3);
+        bar3.value = bar3.value + 10;
+        break;
+      }
+  },
+  handleSubClick: function() {
+    var option = React.findDOMNode(this.refs.select).value;
+    switch(option) {
+      case '1':
+        var bar1 = React.findDOMNode(this.refs.progressBar1);
+        bar1.value = bar1.value - 10;
+        break;
+      case '2':
+        var bar2 = React.findDOMNode(this.refs.progressBar2);
+        bar2.value = bar2.value - 10;
+        break;
+      case '3':
+        var bar3 = React.findDOMNode(this.refs.progressBar3);
+        bar3.value = bar3.value - 10;
+        break;
+      }
+  },
   render: function () {
     return (
         <div className="ProgressBar">
           <h2>Content for ProgressBar</h2>
-              <progress value="28" max="100"></progress>
-              <progress value="18" max="100"></progress>
-              <progress value="78" max="100"></progress>
+          <progress value="20" max="100" ref="progressBar1"></progress>
+          <progress value="50" max="100" ref="progressBar2"></progress>
+          <progress value="40" max="100" ref="progressBar3"></progress>
+          <select ref="select">
+            <option value="1" ref="option1">ProgressBar1</option>
+            <option value="2" ref="option2">ProgressBar2</option>
+            <option value="3" ref="option3">ProgressBar3</option>
+          </select>
+          <input type="button" value="+" onClick={this.handleAddClick} />
+          <input type="button" value="-" onClick={this.handleSubClick} />
         </div>
       );
   }
